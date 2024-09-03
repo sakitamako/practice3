8646447d4479c948518f93ae811c3f96f7346a10
sakitamako <sa.919.1025.ki@gmail.com>
2024-09-03 21:52:29 +0900

    最低限の入力で一旦保存

--- src/com/diworksdev/practice3/action/RegistCompleteAction.java
+++ src/com/diworksdev/practice3/action/RegistCompleteAction.java
@@ -1,5 +1,11 @@
 package com.diworksdev.practice3.action;
 
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.diworksdev.practice3.dao.RegistCompleteDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
 //ユーザー登録機能
@@ -9,13 +15,226 @@ import com.opensymphony.xwork2.ActionSupport;
 //struts2が持つActionSupportというクラスを継承
 //（Actionクラスは基本的にこのクラスを継承）
 //LoginAciton（子クラス） extends（継承） ActionSupport（親クラス）
-public class RegistCompleteAction extends ActionSupport {
+public class RegistCompleteAction extends ActionSupport implements SessionAware {
 
-	//新規ユーザー登録はボタンを押したらサクセスが返される、userCreate.jsp画面に遷移する
-	//struts.xmlで指定しているから！
-	public String execute() {
+	//フィールド変数
+	//JSPから受け取る値
+	//※必ずJSPでの定義と同じ名前にする
+	private String userFamilyName;
+	private String userLastName;
+	private String userFamilyNameKana;
+	private String userLastNameKana;
+	private String userMail;
+	private String userPassword;
+	private String userGender;
+	private String userPostalCode;
+	private String userPrefecture;
+	private String userAddress1;
+	private String userAddress2;
+	private String userAuthority;
 
-		return SUCCESS;
+	//Map<String, Object>=キーを値にマッピングするオブジェクト。
+	//マップには、同一のキーを複数登録できない。各キーは1つの値にしかマッピングできません。
+    //このインタフェースは、インタフェースというよりむしろ完全に抽象クラスであったDictionaryクラスに代わるものです
+	//全てのクラス 変数 変数名
+	public Map<String, Object> session;
+
+	//②userCreateCompleteDAOのインスタンス化（コピーして代入）
+	private RegistCompleteDAO userCreateCompleteDAO = new RegistCompleteDAO();
+
+	//全てのクラス 変数 変数名(struts) throws=例外を意図的に起こすことが出来る処理のこと。
+	public String execute() throws SQLException {
+
+		//DAOを経由して入力された内容をDBに登録します。
+		//DAOのcreateUserに記憶しているid,pass,nameを取得してテキストで表す文字列を返す
+		RegistCompleteDAO.regist(session.get("userFamilyName").toString(), session.get("serLastName").toString(), session.get("userFamilyNameKana").toString(),
+				session.get("userLastNameKana").toString(), session.get("userMail").toString(), session.get("userPassword").toString(),
+				session.get("userGender").toString(), session.get("userPostalCode").toString(), session.get("userPrefecture").toString(),
+				session.get("userAddress1").toString(), session.get("userAddress2").toString(), session.get("userAuthority").toString());
+
+		//変数・文字列 result=SUCCESS
+		//userCreateComplete.jspに遷移する
+		String result = SUCCESS;
+
+		//戻り値
+		//resultに入った値の処理結果をstrutsが取得してuserCreateComplete.jspに遷移する
+		return result;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginUserIdフィールドの値をActionに渡す
+	public String getUserFamilyName() {
+		return userFamilyName;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginUserIdフィールドに格納
+	public void setUserFamilyName(String userFamilyName) {
+		this.userFamilyName = userFamilyName;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginPasswordフィールドの値をActionに渡す
+	public String getUserLastName() {
+		return userLastName;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginPasswordフィールドに格納
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、userNameフィールドの値をActionに渡す
+	public String getUserFamilyNameKana() {
+		return userFamilyNameKana;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のuserNameフィールドに格納
+	public void setUserFamilyNameKana(String userFamilyNameKana) {
+		this.userFamilyNameKana = userFamilyNameKana;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginUserIdフィールドの値をActionに渡す
+	public String getUserLastNameKana() {
+		return userLastNameKana;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginUserIdフィールドに格納
+	public void setUserLastNameKana(String userLastNameKana) {
+		this.userLastNameKana = userLastNameKana;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginPasswordフィールドの値をActionに渡す
+	public String getUserMail() {
+		return userMail;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginPasswordフィールドに格納
+	public void setUserMail(String userMail) {
+		this.userMail = userMail;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、userNameフィールドの値をActionに渡す
+	public String getUserPassword() {
+		return userPassword;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のuserNameフィールドに格納
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginUserIdフィールドの値をActionに渡す
+	public String getUserGender() {
+		return userGender;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginUserIdフィールドに格納
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginPasswordフィールドの値をActionに渡す
+	public String getUserPostalCode() {
+		return userPostalCode;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginPasswordフィールドに格納
+	public void setUserPostalCode(String userPostalCode) {
+		this.userPostalCode = userPostalCode;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、userNameフィールドの値をActionに渡す
+	public String getUserPrefecture() {
+		return userPrefecture;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のuserNameフィールドに格納
+	public void setUserPrefecture(String userPrefecture) {
+		this.userPrefecture = userPrefecture;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginUserIdフィールドの値をActionに渡す
+	public String getUserAddress1() {
+		return userAddress1;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginUserIdフィールドに格納
+	public void setUserAddress1(String userAddress1) {
+		this.userAddress1 = userAddress1;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、loginPasswordフィールドの値をActionに渡す
+	public String getUserAddress2() {
+		return userAddress2;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のloginPasswordフィールドに格納
+	public void setUserAddress2(String userAddress2) {
+		this.userAddress2 = userAddress2;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、userNameフィールドの値をActionに渡す
+	public String getUserAuthority() {
+		return userAuthority;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のuserNameフィールドに格納
+	public void setUserAuthority(String userAuthority) {
+		this.userAuthority = userAuthority;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//全てのクラスのsetの値を自身のsessionフィールドに代入して格納
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
 
 	}
 
---
+++ src/com/diworksdev/practice3/dao/RegistCompleteDAO.java
@@ -0,0 +1,99 @@
+package com.diworksdev.practice3.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.diworksdev.practice2.util.DBConnector;
+import com.diworksdev.practice2.util.DateUtil;
+//ユーザー登録機能
+
+//DAOクラスでは、Actionから送られてきた情報を使ってDBへ問い合わせを行うファイル
+//問い合わせて取得した値をDTOクラスに格納するファイル
+
+public class RegistCompleteDAO {
+
+	// このクラスのみ 変数 変数名
+	// ④sql文を書く：値は ? を入れておく（どんな値でも使いまわしできるようにするため
+	/*
+	 * データベースのテーブル上にデータを登録する際に使用されるステートメントの構文=INSERT INTO 1
+	 * つ以上の行のセットをテーブルとして返す＝VALUES=中身のこと 作成したテーブルに情報を格納する
+	 */
+	/*String sql = "SELECT family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, "
+			+ "postal_code, prefecture, address_1, address_2, authority FROM login_user_transaction "
+			+ "DEFAULT NULL cregistered_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP "
+			+ "update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,";*/
+
+	// 全てのクラス 変数 変数名の中の引数を throws=例外を意図的に起こすことが出来る処理のこと。
+	public static void regist(String userFamily_name, String userLast_name, String userFamily_name_kana, String userLast_name_kana,
+			String userMail, String userPassword, String userPrefecture, String userAddress_1,
+			String userAddress_2) throws SQLException {
+
+		//String gender, String postal_code,
+		//String authority
+
+		DateUtil dateUtil = new DateUtil();
+
+		String sql = "INSERT INTO login_user_transaction (family_name, last_name, family_name_kana, last_name_kana,"
+				+ " mail, password, gender, postal_code, prefecture, address_1, address_2, authority, update_time) "
+				+ "VALUES(?, ? , ?, ?, ?, ?, ?, ?, ?, ?)";
+
+		// ②DBConnectorのインスタンス化
+		// DBへの接続準備、DBと会話するためのコード、これでログインできる
+		// Connectionは特定のデータベースとの接続
+		DBConnector dbConnector = new DBConnector();
+
+		// ③getConnectionの呼び出し（DBと接続する）
+		Connection connection = dbConnector.getConnection();
+
+
+		//このクラスのみ 変数 変数名 インスタンス化（コピーして代入）
+		//DateUtil dateUtil = new DateUtil();
+
+		// try.catchはjavaの例外処理のための構文
+		try {
+
+			// tryの中にはエラーが発生しそうな処理を書く
+			// ⑤PreparedStatement（DBまで運んでくれる箱のイメージ）に代入
+			// 定義したSQL文の1番目の?にActionから送られたname、
+			// 2番目の?にActionから送られたpasswordがそれぞれ入る
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+			// ⑥sql文の?に入れる値をsetする
+			// preparedStatement.setString(1, Id);
+			preparedStatement.setString(1, userFamily_name);
+			preparedStatement.setString(2, userLast_name);
+			preparedStatement.setString(3, userFamily_name_kana);
+			preparedStatement.setString(4, userLast_name_kana);
+			preparedStatement.setString(5, userMail);
+			preparedStatement.setString(6, userPassword);
+			//preparedStatement.setString(7, gender);
+			//preparedStatement.setString(8, postal_code);
+			preparedStatement.setString(9, userPrefecture);
+			preparedStatement.setString(10, userAddress_1);
+			preparedStatement.setString(11, userAddress_2);
+			//preparedStatement.setString(12, authority);
+			preparedStatement.setString(13, dateUtil.getDateTime());
+			preparedStatement.execute();
+
+			// 処理中にSQL関連のエラーが発生した際に実行する処理
+			// tryの中でエラーが発生した場合、catchが受け取り
+			// 例外がスローされる原因となったエラーまたは動作の説明を返す
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			// DB接続を終了する際、必ず書くメソッド
+			// 最後に実行されるものを指定するための構文
+			// 例外が発生しcatchされてもされなくても、共通してやってほしい処理や、やらなければいけない処理を書くところ。
+		} finally {
+
+			// ⑨con.close()で接続を切る
+			// データベースとの接続をクローズ
+			// これをしないとデータベースを接続したまま作業が実行されてしまってメモリに負荷がかかる
+			connection.close();
+
+		}
+
+	}
+
+}
