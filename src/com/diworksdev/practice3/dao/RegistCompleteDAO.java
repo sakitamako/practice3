04e268c19c8bd38115282e2d575bc01eaec440fe
sakitamako <sa.919.1025.ki@gmail.com>
2024-09-18 23:32:58 +0900

    小川講師から教えてもらったところ

--- src/com/diworksdev/practice3/dao/RegistCompleteDAO.java
+++ src/com/diworksdev/practice3/dao/RegistCompleteDAO.java
@@ -15,38 +15,44 @@ import com.diworksdev.practice3.util.DateUtil;
 
 public class RegistCompleteDAO {
 
-	//②DBConnectorのインスタンス化
+	//DBConnectorのインスタンス化
 	//DBへの接続準備、DBと会話するためのコード、これでログインできる
 	//Connectionは特定のデータベースとの接続
 	private DBConnector dbConnector = new DBConnector();
 
-	//③getConnectionの呼び出し（DBと接続する）
+	//getConnectionの呼び出し（DBと接続する）
 	private Connection connection = dbConnector.getConnection();
 
 	//このクラスのみ 変数 変数名 インスタンス化（コピーして代入）
 	private DateUtil dateUtil = new DateUtil();
 
 	//このクラスのみ 変数 変数名
-	//④sql文を書く：値は ? を入れておく（どんな値でも使いまわしできるようにするため
+	//sql文を書く：値は ? を入れておく（どんな値でも使いまわしできるようにするため
 	/*データベースのテーブル上にデータを登録する際に使用されるステートメントの構文=INSERT INTO
  	*1 つ以上の行のセットをテーブルとして返す＝VALUES=中身のこと
  	*作成したテーブルに情報を格納する*/
-	private String sql = "INSERT INTO login_user_transaction("
-			+ "family_name, last_name, family_name_kana, last_name_kana, mail, password, "
-			+ "gender, postal_code, prefecture, address_1, address_2, authority, registered_time) "
-			+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//	private String sql = "INSERT INTO login_user_transaction("
+//			+ "family_name, last_name, family_name_kana, last_name_kana, mail, password, "
+//			+ "gender, postal_code, prefecture, address_1, address_2, authority, registered_time) "
+//			+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+	//①小川講師から教えてもらったこと、下記みたいに１個ずつ試してみる！上記の完成形はコメントアウト！
+	private String sql = "INSERT INTO login_user_transaction(family_name) VALUES(?)";
 
 	//DBから購入履歴を取得するためのメソッド
-	//①クラス、メソッドの定義
+	//クラス、メソッドの定義
 	//DTO型を最後に呼び出し元に渡すので、DTO型を戻り値にしたメソッドを作る
 	//Actionクラスの値を引数として受け取る,throws=例外を意図的に起こすことが出来る処理のこと。
+	//②小川講師から教えてもらったこと、下記は変えなくていい！
 	public void regist(String userFamilyName, String userLastName, String userFamilyNameKana,
 			String userLastNameKana, String userMail, String userPassword,
 			String userGender, String userPostalCode, String userPrefecture,
 			String userAddress1, String userAddress2, String userAuthority) throws SQLException {
 
+		//③小川講師から教えてもらったこと、上記の確認したい項目を入力！
+		System.out.println(userFamilyName);
 	//DBから購入履歴を取得するためのメソッド
-	//①クラス、メソッドの定義
+	//クラス、メソッドの定義
 	//DTO型を最後に呼び出し元に渡すので、DTO型を戻り値にしたメソッドを作る
 	//Actionクラスの値を引数として受け取る,throws=例外を意図的に起こすことが出来る処理のこと。
 //	public ArrayList<HomeDTO> getRegist(String userFamilyName, String userLastName, String userFamilyNameKana,
@@ -64,25 +70,28 @@ public class RegistCompleteDAO {
 		try {
 
 			//tryの中にはエラーが発生しそうな処理を書く
-			//⑤PreparedStatement（DBまで運んでくれる箱のイメージ）に代入
+			//PreparedStatement（DBまで運んでくれる箱のイメージ）に代入
 			//定義したSQL文の1番目の?にActionから送られたname、
 			//2番目の?にActionから送られたpasswordがそれぞれ入る
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-			//⑥sql文の?に入れる値をsetする
+			//④小川講師から教えてもらったこと、確認したい項目だけに絞る、それ以外コメントアウト！
+			//④までやったら実行確認してphpMyAdminに値が入っているか確認する！必ずコミットプッシュする
+			//インサートしている値とデータベースのカラムの型が違う可能性あり！
+			//sql文の?に入れる値をsetする
 			preparedStatement.setString(1, userFamilyName);
-			preparedStatement.setString(2, userLastName);
-			preparedStatement.setString(3, userFamilyNameKana);
-			preparedStatement.setString(4, userLastNameKana);
-			preparedStatement.setString(5, userMail);
-			preparedStatement.setString(6, userPassword);
-			preparedStatement.setString(7, userGender);
-			preparedStatement.setString(8, userPostalCode);
-			preparedStatement.setString(9, userPrefecture);
-			preparedStatement.setString(10, userAddress1);
-			preparedStatement.setString(11, userAddress2);
-			preparedStatement.setString(12, userAuthority);
-			preparedStatement.setString(13, dateUtil.getDate());
+//			preparedStatement.setString(2, userLastName);
+//			preparedStatement.setString(3, userFamilyNameKana);
+//			preparedStatement.setString(4, userLastNameKana);
+//			preparedStatement.setString(5, userMail);
+//			preparedStatement.setString(6, userPassword);
+//			preparedStatement.setString(7, userGender);
+//			preparedStatement.setString(8, userPostalCode);
+//			preparedStatement.setString(9, userPrefecture);
+//			preparedStatement.setString(10, userAddress1);
+//			preparedStatement.setString(11, userAddress2);
+//			preparedStatement.setString(12, userAuthority);
+//			preparedStatement.setString(13, dateUtil.getDate());
 			preparedStatement.execute();
 
 			//⑦executeQuery()/executeUpdate()で実行
