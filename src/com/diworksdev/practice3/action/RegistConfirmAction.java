467b28734cf4c314520d2c3d1201fd637562b260
sakitamako <sa.919.1025.ki@gmail.com>
2024-10-08 22:52:49 +0900

    入力文字種類の判定できた！

--- src/com/diworksdev/practice3/action/RegistConfirmAction.java
+++ src/com/diworksdev/practice3/action/RegistConfirmAction.java
@@ -74,47 +74,63 @@ public class RegistConfirmAction extends ActionSupport implements SessionAware {
 		if (userLastName == null || userLastName.isEmpty()) {
 			addFieldError("userLastName", "名前（名）が未入力です。");
 
-		}
+		} else if (!(userLastName.matches("^[\\p{IsHan}\\p{IsHiragana}]+$"))){
+            addFieldError("userLastName", "名前（名）は平仮名と漢字のみ使用できます。");
+        }
 
 		if (userFamilyNameKana == null || userFamilyNameKana.isEmpty()) {
 			addFieldError("userFamilyNameKana", "カナ（姓）が未入力です。");
 
-		}
+		} else if (!(userFamilyNameKana.matches("^[\\p{IsKatakana}ー]+$"))){
+            addFieldError("userFamilyNameKana", "カナ（姓）はカタカナのみ使用できます。");
+        }
 
 		if (userLastNameKana == null || userLastNameKana.isEmpty()) {
 			addFieldError("userLastNameKana", "カナ（名）が未入力です。");
 
-		}
+		} else if (!(userLastNameKana.matches("^[\\p{IsKatakana}ー]+$"))){
+            addFieldError("userLastNameKana", "カナ（名）はカタカナのみ使用できます。");
+        }
 
 		if (userMail == null || userMail.isEmpty()) {
 			addFieldError("userMail", "メールアドレスが未入力です。");
 
-		}
+		} else if (!(userMail.matches("^[a-zA-Z0-9-@.]+$"))){
+            addFieldError("userMail", "メールアドレスは半角英数字、半角ハイフン、半角記号（ハイフンとアットマークとドット）のみ使用できます。");
+        }
 
 		if (userPassword == null || userPassword.isEmpty()) {
 			addFieldError("userPassword", "パスワードが未入力です。");
 
-		}
+		} else if (!(userPassword.matches("^[a-zA-Z0-9]+$"))){
+            addFieldError("userPassword", "パスワードは半角英数字のみ使用できます。");
+        }
 
 		if (userPostalCode0 == null || userPostalCode0.isEmpty()) {
 			addFieldError("userPostalCode0", "郵便番号が未入力です。");
 
-		}
+		} else if (!(userPostalCode0.matches("^[0-9]+$"))){
+            addFieldError("userPostalCode0", "郵便番号は半角数字のみ使用できます。");
+        }
 
 		if (userPrefecture == null || userPrefecture.isEmpty()) {
 			addFieldError("userPrefecture", "住所（都道府県）が未入力です。");
 
-		}
+        }
 
 		if (userAddress1 == null || userAddress1.isEmpty()) {
 			addFieldError("userAddress1", "住所（市区町村）が未入力です。");
 
-		}
+		} else if (!(userAddress1.matches("^[\\p{IsHiragana}\\p{IsHan}\\p{IsKatakana}0-9\\s-]+$"))){
+            addFieldError("userAddress1", "住所（市区町村）はひらがな、漢字、カタカナ、半角数字、ハイフン、スペースのみ使用できます。");
+        }
 
 		if (userAddress2 == null || userAddress2.isEmpty()) {
 			addFieldError("userAddress2", "住所（番地）が未入力です。");
 
-		}
+		} else if (!(userAddress2.matches("^[\\p{IsHiragana}\\p{IsHan}\\p{IsKatakana}0-9\\s-]+$"))){
+            addFieldError("userAddress2", "住所（番地）はひらがな、漢字、カタカナ、半角数字、ハイフン、スペースのみ使用できます。");
+        }
 
 	}
 
